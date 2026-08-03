d3ea97f3afddbc429d0a7601d374f4d66d0042a4
이종수 <81428357+Lee-JongSoo@users.noreply.github.com>
2026-08-03 22:02:30 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.01 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        
+        if (num1 > 100 || num2 > 100) return 0
+        
+        return num1 * num2
+    }
+}
